8161fcece777c955a225623c9653009ede537a36
josedavidgf <josedavidgarciafernandez@gmail.com>
2025-05-01 23:21:04 +0100

    Update supabase

--- api/src/config/supabase.js
+++ api/src/config/supabase.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_API_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.SUPABASE_API_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Supabase URL and API key are required.');
